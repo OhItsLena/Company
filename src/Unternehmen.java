2efbce6e6ca2d645af35328a57b58d151b08d15c
OhItsLena <lena.hammerer@vol.at>
2021-08-02 12:36:37 +0200

    Added logic to manage Employees
    Change-Id: Ie6f26aa31795569d5045970e2cb5a43a59afd820

--- src/Unternehmen.java
+++ src/Unternehmen.java
@@ -1,5 +1,7 @@
+import java.util.ArrayList;
 
 public class Unternehmen {
+	private ArrayList<Mitarbeiter> mitarbeiter = new ArrayList<Mitarbeiter>();
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
@@ -7,10 +9,10 @@ public class Unternehmen {
 	}
 
 	public void mitarbeiterEinstellen(Mitarbeiter m) {
-
+		mitarbeiter.add(m);
 	}
 
 	public void mitarbeiterKündigen(Mitarbeiter m) {
-
+		mitarbeiter.remove(m);
 	}
 }
